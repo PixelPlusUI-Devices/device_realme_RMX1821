63f8ba5194d98cb78373c241b989546301d508d5
DrtSinX98 <pritish@stag-os.org>
2021-06-20 11:57:12 +0200

    PPUI-ify

--- aosp_RMX1821.mk
+++ aosp_RMX1821.mk
@@ -18,16 +18,30 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common PPUI stuff
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+TARGET_FACE_UNLOCK_SUPPORTED := true
+CUSTOM_BUILD_TYPE := OFFICIAL
+TARGET_SUPPORTS_GOOGLE_RECORDER := false
+TARGET_INCLUDE_STOCK_ACORE := false
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_BOOT_ANIMATION_RES := 720
 
 # Inherit from Realme RMX1821
 $(call inherit-product, device/realme/RMX1821/device.mk)
 
-PRODUCT_NAME := lineage_RMX1821
+PRODUCT_NAME := aosp_RMX1821
 PRODUCT_DEVICE := RMX1821
 PRODUCT_MANUFACTURER := Realme
 PRODUCT_BRAND := Realme
 PRODUCT_MODEL := RMX1821
 
+# PixelPlusUI OFFICIAL
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.ppui.device_name=Realme3 \
+    ro.ppui.version=3.6 \
+    ro.ppui.version_code=Unicorn \
+    ro.ppui.is_official=true \
+    ro.ppui.maintainer_name=Pritish
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
